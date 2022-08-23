fbb76c54bf4025aaff79cfec42325441f7387e12
AsadCus <icbpunvr2019@gmail.com>
2022-08-23 15:02:14 +0700

    :sparkles: add history price

---
+++ app/Services/HistorypriceService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Historyprice;
+use Illuminate\Http\Request;
+// use App\Services\HistorypriceService;
+use Illuminate\Support\Facades\Validator;
+
+class HistorypriceService
+{
+
+    public function __construct(Historyprice $hp)
+    {
+        $this->hp = $hp;
+    }
+    
+    public function handleIndex()
+    {
+        $hps = $this->hp->all();
+
+        return view('part.detail', [
+            'hps' => $hps
+        ]);
+        // return($hps);
+    }
+
+    public function handleStore(Request $request)
+    {
+        $this->hp->create($request->all());
+        return redirect('/detail/part');
+    }
+
+}
