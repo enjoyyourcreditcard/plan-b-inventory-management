113d31fc90cf6e2db59f4296b433f8205d03cd25
hajidalakhtar <hajidalakhtar@gmail.com>
2022-10-31 10:33:53 +0700

    backup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,14 +25,11 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset("dist/css/app.css")}}" />
 
 
-    @viteReactRefresh
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{asset("dist/css/app.css")}}" />
-    {{-- <link href="" rel="stylesheet"> --}}
-
 
 </head>
 <!-- END: Head -->
